09147bc8fddadc9315e923bdec8467c607851a71
julilan <juli.molnar@outlook.com>
2023-11-14 01:07:11 +0200

    remove uppercase and font-bold, add conditional rendering for subitems

--- next/components/main-menu/main-menu-full.tsx
+++ next/components/main-menu/main-menu-full.tsx
@@ -8,22 +8,24 @@ export function MainMenuFull({ menu }) {
       >
         {menu.map((item) => (
           <div className="relative group">
-            <button className="flex flex-row items-center w-full px-4 py-4 mt-2 text-base font-bold text-left uppercase bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 focus:outline-none font-montserrat hover:underline">
+            <button className="flex flex-row items-center w-full px-4 py-4 mt-2 text-base text-left bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 focus:outline-none font-montserrat hover:underline">
               <Link href={item.url}>
                 <span>{item.title}</span>
               </Link>
             </button>
-            <div className="absolute z-10 hidden bg-primary-500 group-hover:block">
-              <div className="px-4 pt-2 pb-4 bg-white shadow-lg w-40">
-                <div className="grid grid-cols-1 gap-4">
-                  {item.items?.map((subitem) => (
-                    <Link href={subitem.url}>
-                      <li className="hover:underline">{subitem.title}</li>
-                    </Link>
-                  ))}
+            {item.items && item.items.length > 0 && (
+              <div className="absolute z-10 hidden bg-primary-500 group-hover:block">
+                <div className="px-4 pt-2 pb-4 bg-white shadow-lg w-40">
+                  <div className="grid grid-cols-1 gap-4">
+                    {item.items.map((subitem) => (
+                      <Link href={subitem.url}>
+                        <li className="hover:underline">{subitem.title}</li>
+                      </Link>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         ))}
       </nav>
