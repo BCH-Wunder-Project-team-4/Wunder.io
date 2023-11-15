c7fcb279c6f4224158e4f86425e7d6bee661d14c
julilan <juli.molnar@outlook.com>
2023-11-15 17:04:21 +0200

    remove one inner div, adjust submenu width

--- next/components/main-menu/main-menu-full.tsx
+++ next/components/main-menu/main-menu-full.tsx
@@ -52,23 +52,21 @@ export function MainMenuFull({ menu }: MainMenuFullProps) {
                 <div
                   className={`absolute z-10 ${
                     visibleSubmenu === item.id ? "block" : "hidden"
-                  } bg-primary-500`}
+                  } px-4 pt-2 pb-4 bg-white shadow-lg w-50`}
                 >
-                  <div className="px-4 pt-2 pb-4 bg-white shadow-lg w-60">
-                    <ul className="grid grid-cols-1 gap-4">
-                      {item.items.map((subitem) => (
-                        <li key={subitem.id}>
-                          <Link
-                            href={subitem.url}
-                            className="hover:underline"
-                            onClick={() => setVisibleSubmenu(null)}
-                          >
-                            {subitem.title}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
+                  <ul className="grid grid-cols-1 gap-4">
+                    {item.items.map((subitem) => (
+                      <li key={subitem.id}>
+                        <Link
+                          href={subitem.url}
+                          className="hover:underline block"
+                          onClick={() => setVisibleSubmenu(null)}
+                        >
+                          {subitem.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
               )}
             </li>
