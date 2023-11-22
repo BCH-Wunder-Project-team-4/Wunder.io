309910a06f16a747d34781f2279ebb2494036fa2
markusVJH <markus.heinila@pp.inet.fi>
2023-11-22 09:13:30 +0200

    logos centered

--- next/components/logo-wall.tsx
+++ next/components/logo-wall.tsx
@@ -10,7 +10,7 @@ interface LogoWallProps {
 
 export function LogoWall({ clients }: LogoWallProps) {
   return (
-    <div className="flex flex-wrap justify-center">
+    <div className="flex flex-wrap justify-center items-center">
       {clients.map((client) => (
         <Link  href={client.field_link.full_url} target="_blank" rel="noopener noreferrer">
           <div className="p-4">
