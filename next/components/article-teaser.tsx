c31e69b356091ae16f391f8f1b3a912104cd04dc
markusVJH <markus.heinila@pp.inet.fi>
2023-11-23 13:13:57 +0200

    darkmode/lightmode setup, basic colors set

--- next/components/article-teaser.tsx
+++ next/components/article-teaser.tsx
@@ -19,7 +19,7 @@ export function ArticleTeaser({ article }: ArticleTeaserProps) {
   return (
     <Link
       href={article.path.alias}
-      className="relative grid h-full rounded border dark:border-white border-primary-600 dark:bg-steelgray bg-mischka p-4 transition-all hover:shadow-md"
+      className="relative grid h-full rounded border dark:border-scapaflow border-primary-600 dark:bg-steelgray bg-mischka p-4 transition-all hover:shadow-md"
     >
       <h3 className="mb-2 line-clamp-2 text-heading-xs font-bold">
         {article.title}
--- next/components/formatted-text.tsx
+++ next/components/formatted-text.tsx
@@ -60,7 +60,7 @@ const options: HTMLReactParserOptions = {
       }
 
       case "p": {
-        return <p className="mb-2">{domToReact(domNode.children, options)}</p>;
+        return <p className="mb-2 text-steelgray dark:text-mischka">{domToReact(domNode.children, options)}</p>;
       }
 
       case "input": {
@@ -83,5 +83,5 @@ interface FormattedTextProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export function FormattedText({ html, ...props }: FormattedTextProps) {
-  return <div {...props}>{parse(html, options)}</div>;
+  return <div {...props} >{parse(html, options)}</div>;
 }
