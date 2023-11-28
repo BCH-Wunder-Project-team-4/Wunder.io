2f055e8dfcd666252b32c5e758e05e03ecd6be77
julilan <juli.molnar@outlook.com>
2023-11-29 01:55:34 +0200

    add framer-motion library and scrolling number effect

--- next/components/paragraph--scrolling-numbers.tsx
+++ next/components/paragraph--scrolling-numbers.tsx
@@ -1,4 +1,14 @@
 import { ScrollingNumbers } from "@/lib/zod/paragraph";
+import { HeadingParagraph } from "./heading--paragraph";
+
+import {
+  animate,
+  motion,
+  useInView,
+  useMotionValue,
+  useTransform,
+} from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export function ParagraphScrollingNumbers({
   paragraph,
@@ -6,20 +16,52 @@ export function ParagraphScrollingNumbers({
   paragraph: ScrollingNumbers;
 }) {
   if (!paragraph.field_scrolling_numbers_items?.length) return null;
-  // console.log(paragraph);
+
+  const Counter = ({ from, to, duration }) => {
+    const count = useMotionValue(from);
+    const ref = useRef(null);
+    // const { scrollYProgress } = useScroll();
+    const isInView = useInView(ref, { once: true });
+    const [hasAnimated, setHasAnimated] = useState(false);
+    const rounded = useTransform(count, (latest) => Math.round(latest));
+
+    useEffect(() => {
+      if (isInView && !hasAnimated) {
+        animate(count, to, { duration: duration });
+        setHasAnimated(true);
+      }
+    }, [count, to, duration, isInView, hasAnimated]);
+
+    return (
+      <motion.p
+        className="font-bold text-2xl"
+        ref={ref}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
+        {rounded}
+      </motion.p>
+    );
+  };
 
   return (
-    <div className="flex flex-col items-center">
-      <h2>{paragraph.field_heading}</h2>
+    <section id="scrolling_numbers">
+      <HeadingParagraph>{paragraph.field_heading}</HeadingParagraph>
+      <div className="h-36"></div>
       <div className="flex flex-row">
         {paragraph.field_scrolling_numbers_items.map((item, index) => (
           <div key={index} className="flex flex-col items-center">
-            <div className="font-bold text-5xl">{item.field_number}</div>
-            <div className="text-2xl">{item.field_number_suffix}</div>
+            <Counter from={0} to={item.field_number} duration={2} />
+            {item.field_number_suffix}
+            {/* <motion.p className="font-bold text-2xl">
+              {item.field_number}
+              {item.field_number_suffix}
+            </motion.p> */}
             <div className="text-2xl">{item.field_description}</div>
           </div>
         ))}
       </div>
-    </div>
+      <div className="h-36"></div>
+    </section>
   );
 }
