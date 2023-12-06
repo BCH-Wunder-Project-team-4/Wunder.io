d5bb03afeba92ccab3c805f5613388d123e3a70c
markusVJH <markus.heinila@pp.inet.fi>
2023-12-06 17:05:54 +0200

    anchor navigation styling

--- next/components/page.tsx
+++ next/components/page.tsx
@@ -1,6 +1,7 @@
 import { Paragraph } from "@/components/paragraph";
 import type { Page } from "@/lib/zod/page";
 import { Anchor } from "@/lib/zod/paragraph";
+import Chevron from "@/styles/icons/chevron-down.svg";
 import Link from "next/link";
 
 interface PageProps {
@@ -15,20 +16,23 @@ export function Page({ page }: PageProps) {
   return (
     <>
       {anchors.length > 0 && (
-        <div className="flex gap-6 border-b mb-9">
-          <span>Jump to: </span>
+        <div className="mx-2 border-t border-primary-600 dark:border-fog mb-9">
+          <div className="pt-4 flex gap-4">
+          <span>Skip to: </span>
           <ul className="flex gap-4">
             {anchors?.map((anchor) => (
-              <li key={anchor.id} className="text-secondary-foreground">
+              <li key={anchor.id}>
                 <Link
                   href={`#${anchor.field_section_id}`}
-                  className="hover:underline"
-                >
-                  {anchor.field_section_id}
+                  className="hover:underline text-primary-600 dark:text-fog flex items-center"
+                  >
+                    <Chevron className="w-6 h-6 mr-2 text-steelgray dark:text-mischka"/>
+                    {anchor.field_section_id}
                 </Link>
               </li>
             ))}
           </ul>
+          </div>
         </div>
       )}
     <div className="grid gap-4">
