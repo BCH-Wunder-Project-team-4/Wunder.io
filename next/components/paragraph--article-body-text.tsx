5a26a6c66bf513a67260b90ad4f0398b7d4d80cc
julilan <juli.molnar@outlook.com>
2023-12-11 19:49:27 +0200

    remove text-colors

--- next/components/paragraph--article-body-text.tsx
+++ next/components/paragraph--article-body-text.tsx
@@ -11,7 +11,7 @@ export function ParagraphArticleBodyText({
       {paragraph.field_formatted_text.processed && (
         <FormattedText
           html={paragraph.field_formatted_text.processed}
-          className="text-left text-md/xl dark:text-graysuit text-scapaflow sm:text-lg mt-4"
+          className="text-left text-md/xl sm:text-lg mt-4"
         />
       )}
     </>
