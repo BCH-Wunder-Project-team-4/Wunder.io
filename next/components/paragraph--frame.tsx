7380f2dd27a019052a61d7ee543c57965bc8940a
markusVJH <markus.heinila@pp.inet.fi>
2023-12-12 14:10:49 +0200

    h-full

--- next/components/paragraph--frame.tsx
+++ next/components/paragraph--frame.tsx
@@ -62,7 +62,7 @@ export function ParagraphFrame({ paragraph }: { paragraph: FrameType }) {
             <div className="h-[34.5px] w-[3px] bg-scapaflow dark:bg-scapaflow absolute -start-[17px] top-[93px] rounded-s-lg"></div>
             <div className="h-[34.5px] w-[3px] bg-scapaflow dark:bg-scapaflow absolute -start-[17px] top-[133.5px] rounded-s-lg"></div>
             <div className="h-[48px] w-[3px] bg-scapaflow dark:bg-scapaflow absolute -end-[17px] top-[106.5px] rounded-e-lg"></div>
-            <div className="rounded-[2rem] overflow-hidden bg-white dark:bg-scapaflow w-[272px] h-[572px]">
+            <div className="rounded-[2rem] overflow-hidden bg-white dark:bg-scapaflow h-full">
               <MediaImage
                 media={paragraph.field_image}
                 alt="site-banner"
