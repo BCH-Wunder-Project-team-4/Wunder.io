3d50a100bf9090ba5cc70cb427c493c64b80f9e2
markusVJH <markus.heinila@pp.inet.fi>
2023-12-13 12:57:22 +0200

    cookie banner styling, decline option

--- next/components/cookie-bar.tsx
+++ next/components/cookie-bar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Cookies from 'js-cookie'
 import { MouseEvent, useCallback, useEffect, useState } from 'react'
+import { Button } from '@/ui/button'
 
 const USER_CONSENT_COOKIE_KEY = 'cookie_consent_is_true'
 const USER_CONSENT_COOKIE_EXPIRE_DATE = 365
@@ -13,7 +14,7 @@ const CookieConsent = () => {
     setCookieConsentIsTrue(consentIsTrue)
   }, [])
 
-  const onClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const onAccept = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
 
     if (!cookieConsentIsTrue) {
@@ -23,38 +24,65 @@ const CookieConsent = () => {
       setCookieConsentIsTrue(true)
     }
   }
+  const onDecline = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    document.getElementById('cookie-consent-modal').style.display = 'none';
+  };
 
   if (cookieConsentIsTrue) {
     return null
   }
 
   return (
-    <section className="fixed bottom-0 left-0 w-full py-2 md:py-4">
-        <div className="flex flex-col items-start px-5 py-3 space-y-2 bg-gray-200 md:flex-row md:space-y-0 md:items-stretch md:space-x-2">
-          <div className="flex items-center flex-grow text-gray-900">
-            <p className="text-sm font-medium">
-              This site uses services that use cookies to deliver better
-              experience and analyze traffic. You can learn more about the
+    <div
+      id="cookie-consent-modal"
+      data-modal-backdrop="static"
+      className="fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center z-50 bg-opacity-15 bg-black backdrop-blur-sm "
+    >
+      <div className="relative p-4 w-full max-w-2xl text-center">
+        <div className="relative bg-steelgray rounded-lg shadow bg-opacity-95">
+          <div className="flex items-center justify-center p-4 md:p-5 border-b border-fog rounded-t">
+            <h3 className="text-xl font-semibold text-finnishwinter">
+              Cookie Consent
+            </h3>
+          </div>
+          <div className="p-4 md:p-5 space-y-4">
+            <p className="text-base leading-relaxed text-finnishwinter">
+              We use services that use cookies to deliver a better user
+              experience, and to analyze traffic. You can learn more about the
               services we use at our{' '}
-              <Link 
-              href="/privacy-policy"
-              className='text-sm underline hover:text-lightAccent'>
-              privacy policy
+              <Link
+                href="/privacy-policy"
+                className="text-fog underline hover:text-blue-800"
+              >
+                privacy policy
               </Link>
-              .
+              {' '} page.
             </p>
           </div>
-          <div className="flex items-center">
-            <button
-              className="p-3 text-sm font-bold text-white uppercase bg-gray-700 whitespace-nowrap"
-              onClick={onClick}
+          <div className="flex items-center justify-center gap-4 p-4 md:p-5 border-t border-fog rounded-b">
+            <Button
+              data-modal-hide="cookie-consent-modal"
+              type="button"
+              onClick={onDecline}
+              className='bg-steelgray'
             >
-              Got it
-            </button>
+              Decline
+            </Button>
+            <Button
+              data-modal-hide="cookie-consent-modal"
+              type="button"
+              onClick={onAccept}
+            >
+              I accept
+            </Button>
           </div>
         </div>
-    </section>
+      </div>
+    </div>
   )
 }
+
 
 export default CookieConsent
