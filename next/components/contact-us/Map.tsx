1c9b023c3953dd3226d2474c17ce298e75ba076e
julilan <juli.molnar@outlook.com>
2023-12-14 18:17:35 +0200

    add attribution link to OpenStreetMap

--- next/components/contact-us/Map.tsx
+++ next/components/contact-us/Map.tsx
@@ -25,7 +25,7 @@ const Map = ({ markers }) => {
       scrollWheelZoom={false}
     >
       <TileLayer
-        attribution="basic"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
