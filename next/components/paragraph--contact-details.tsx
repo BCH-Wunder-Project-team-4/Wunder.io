90977fbb71535617d2264e8186bcbdff2b0c4e8e
julilan <juli.molnar@outlook.com>
2023-12-18 13:20:58 +0200

    styling for contact details

--- next/components/paragraph--contact-details.tsx
+++ next/components/paragraph--contact-details.tsx
@@ -11,20 +11,26 @@ export function ParagraphContactDetails({
 
   return (
     <section id="contact_details">
-      <HeadingParagraph>{paragraph.field_heading}</HeadingParagraph>
-      <div>
+      {paragraph.field_heading && (
+        <HeadingParagraph>{paragraph.field_heading}</HeadingParagraph>
+      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {paragraph.field_contact_data.map((contact) => (
-          <div key={contact.field_name}>
+          <div className="text-left" key={contact.field_name}>
             <MediaImage
               media={contact.field_image}
               alt="contact-image"
               priority
-              className="w-48 h-48 rounded-md"
+              className="min-h-[200px] object-cover"
             />
-            <h4>{contact.field_name}</h4>
-            <p>{contact.field_position}</p>
-            <p>{contact.field_email}</p>
-            <p>{contact.field_phone}</p>
+            <h4 className="font-bold text-md mt-2 mb-1">
+              {contact.field_name}
+            </h4>
+            <p className="text-xs mb-1 uppercase text-scapaflow">
+              {contact.field_position}
+            </p>
+            {contact.field_email && <p>{contact.field_email}</p>}
+            {contact.field_phone && <p>{contact.field_phone}</p>}
           </div>
         ))}
       </div>
