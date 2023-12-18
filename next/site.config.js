55b5bb9c887970a5118fd9dbcda3ba07d92cb370
markusVJH <markus.heinila@pp.inet.fi>
2023-12-18 20:43:13 +0200

    remove Swedish, add Latvian and Estonian

--- next/site.config.js
+++ next/site.config.js
@@ -11,10 +11,15 @@ const siteConfig = {
       path: "/fi",
       langcode: "fi",
     },
-    sv: {
-      name: "Svenska",
-      path: "/sv",
-      langcode: "sv",
+    lv: {
+      name: "Latviešu",
+      path: "/lv",
+      langcode: "lv",
+    },
+    et: {
+      name: "Eesti",
+      path: "/et",
+      langcode: "et",
     },
   },
 };
